b748e67b075d9aa23439ec0707d26cda6665b2c1
Tijunel <justintijunel@gmail.com>
2020-01-20 18:47:31 -0700

    Small UI changes

--- client/src/mobile/componentsM/dashComponentsM/dashboardM.js
+++ client/src/mobile/componentsM/dashComponentsM/dashboardM.js
@@ -13,28 +13,28 @@ export default class StreamingDashM extends React.Component {
     render = () => {
         if (this.props.dashOption === 'default' && this.props.typeOption === 'plotting') {
             return (
-                <div id='dashboard'>
+                <div id='dashboardM'>
                     <DefaultPlottingDashboardM />
                 </div>
             );
         }
         else if (this.props.dashOption === 'default' && this.props.typeOption === 'currentData') {
             return (
-                <div id='dashboard'>
+                <div id='dashboardM'>
                     <DefaultDataDashM />
                 </div>
             );
         }
         else if (this.props.dashOption === 'custom' && this.props.typeOption === 'plotting') {
             return (
-                <div id='dashboard'>
+                <div id='dashboardM'>
                     <CustomPlotSelectorM />
                 </div>
             );
         }
         else {
             return (
-                <div id='dashboard'>
+                <div id='dashboardM'>
                     <CustomDataSelectorM />
                 </div>
             );

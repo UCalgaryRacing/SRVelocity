b763877a1839b53b99ffcf3537935af196fcab70
Tijunel <justintijunel@gmail.com>
2020-04-18 21:54:32 -0600

    Added build folder

---
+++ client/build/precache-manifest.8dfef728c63c65ad207be53d43c8cc53.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04b935c9f68554aef750aae7a78fbba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89425a0fbccc70396c4f",
+    "url": "/static/css/2.4b57d5a8.chunk.css"
+  },
+  {
+    "revision": "5dfca5d2df8a622aa8f2",
+    "url": "/static/css/main.4372b303.chunk.css"
+  },
+  {
+    "revision": "89425a0fbccc70396c4f",
+    "url": "/static/js/2.2190e6f9.chunk.js"
+  },
+  {
+    "revision": "63ad13538403e1530a40b59590b3c9f1",
+    "url": "/static/js/2.2190e6f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dfca5d2df8a622aa8f2",
+    "url": "/static/js/main.0bc829b4.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "c679e68be489ae6150b691f9e6bfaecc",
+    "url": "/static/media/analysis.c679e68b.svg"
+  },
+  {
+    "revision": "fe27c4109bb859d6a5f8da7deaa24fa6",
+    "url": "/static/media/f1.fe27c410.svg"
+  },
+  {
+    "revision": "77690dad325b1a046b0b1879443ebaef",
+    "url": "/static/media/logo.77690dad.svg"
+  },
+  {
+    "revision": "64c80c67a10344bafba87b399d589878",
+    "url": "/static/media/plot.64c80c67.svg"
+  },
+  {
+    "revision": "7c5146ef518205dd5c60bb4c6dc4591f",
+    "url": "/static/media/streaming.7c5146ef.svg"
+  }
+]);

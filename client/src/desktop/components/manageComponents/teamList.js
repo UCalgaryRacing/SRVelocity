fd404d35c7aa648688ed1431cbbe4573c5a15b75
Saferer <james.nguy@shaw.ca>
2020-05-20 17:00:13 -0600

    Display list of members

--- client/src/desktop/components/manageComponents/teamList.js
+++ client/src/desktop/components/manageComponents/teamList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Row, Col, Table } from "react-bootstrap";
 
 class TeamList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { memberRender: [] };
   }
 
   async componentDidMount() {
@@ -14,6 +15,7 @@ class TeamList extends React.Component {
   async createTeamMemberList() {
     const members = await this.fetchTeamMembers();
     console.log(members);
+    await this.renderTeamMembers(members);
   }
 
   fetchTeamMembers = async () => {
@@ -35,8 +37,31 @@ class TeamList extends React.Component {
     }
   };
 
+  renderTeamMembers = async (members) => {
+    let render = [];
+    members.forEach((element) => {
+      render.push(
+        <tr>
+          <td>{element.first_name}</td>
+          <td>{element.last_name}</td>
+          <td>{element.email}</td>
+          <td>{element.subteam_name}</td>
+        </tr>
+      );
+      console.log(element.first_name);
+    });
+    this.setState({ memberRender: render });
+    console.log(render);
+  };
+
   render() {
-    return <React.Fragment />;
+    return (
+      <React.Fragment>
+        <Table striped hover>
+          <tbody>{this.state.memberRender}</tbody>
+        </Table>
+      </React.Fragment>
+    );
   }
 }
 

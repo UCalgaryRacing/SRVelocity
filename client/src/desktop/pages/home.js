b78b62b543834bbebbf8fa89358f5df066bcd946
Jeremy Bilic <41878670+JeremyBilic@users.noreply.github.com>
2020-06-04 01:06:38 -0600

    Fixed home page

--- client/src/desktop/pages/home.js
+++ client/src/desktop/pages/home.js
@@ -12,39 +12,39 @@ export default class HomePage extends React.Component {
         <React.Fragment>
           <TopNav />
           <div id="homePage">
-            <Row id="row1">
+            <Row id="row1" style={{margin: 'auto'}}>
               <Col style={{ marginTop: "100px" }}>
                 <img
                   style={{
                     width: "100%",
                     textAlign: "center",
-                    marginBottom: "10px",
+                    marginBottom: "20px",
                   }}
                   src={require("../../assets/logo.svg")}
                   alt="Schulich Racing"
                 />
               </Col>
             </Row>
-            <Row id="row2">
+            <Row id="row2" style={{margin: 'auto'}}>
               <Col>
                 <p>
-                  <b style={{ fontSize: "30px" }}>
+                  <b style={{ fontSize: "30px", marginBottom:"20px" }}>
                     Welcome to Schulich Velocity!
                   </b>
                 </p>
-                <p>
+                <p style={{ fontSize: "15px", marginLeft:'5px', marginRight:'5px', marginBottom:'30px'}}>
                   Schulich Velocity is telemetry software created for the SR21.
                   It is capable of receiving data from the SR21 at 60 Hz. This
                   data is stored, plotted, and analyzed in various ways to help
                   us make data driven decisions.
                 </p>
-                <Button id="streamingButton" href="/streaming">
+                <Button id="streamingButton" href="/streaming" >
                   <b>Go to Streaming</b>
                 </Button>
               </Col>
             </Row>
+            <BottomNav />
           </div>
-          <BottomNav />
         </React.Fragment>
       );
     } else

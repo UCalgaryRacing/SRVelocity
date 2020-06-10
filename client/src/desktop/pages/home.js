6a34717f661f794ac2a03b5a420664519e90c4c9
Tijunel <justintijunel@gmail.com>
2020-06-10 01:16:13 -0600

    Final commit

--- client/src/desktop/pages/home.js
+++ client/src/desktop/pages/home.js
@@ -24,14 +24,17 @@ export default class HomePage extends React.Component {
 								<b>Welcome to Schulich Velocity!</b>
 							</p>
 							<p id="infoPara" style={{fontSize: 'calc(0.9vw + 0.4vh + 0.8em)', textAlign: 'justify'}}>
-								Schulich Velocity is telemetry software created for the SR21.
-								It is capable of receiving data from the SR21 at 60 Hz. This
+								Schulich Velocity is telemetry software created for the SR-21.
+								It is capable of receiving data from the SR-21 at 60 Hz. This
 								data is stored, plotted, and analyzed in various ways to help
-								us make data driven decisions.
+								us make data driven decisions. We also have an
+								internal system for documenting and managing our data, along with a plug and play sensor functionality on our 
+								vehicle.
                 			</p>
 							<Button id="streamingButton" href="/streaming" style={{marginTop: '30px', width: '100%', fontSize: 'calc(0.9vw + 0.4vh + 0.8em)'}}>
 								<b>Go to Streaming</b>
 							</Button>
+							<img src={require('../../assets/render.png')} style={{marginTop: '120px', width: '100%'}}/>
 						</Col>
 					</Row>
 				</Jumbotron>

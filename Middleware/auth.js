b321c09d7f7a282e95f84cae9bad310109297b90
Saferer <james.nguy@shaw.ca>
2020-09-19 15:20:00 -0600

    Update auth.js

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -18,7 +18,7 @@ const withAnyAuth = async (req, res, next) => {
         return;
       })
       .catch((error) => {
-        res.status(401).send({ error: "Not Authorized!" }).end();
+        res.status(401).send({ error: "Not authorized!" }).end();
         return;
       });
   } else {
